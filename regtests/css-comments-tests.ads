618d24ea186254730401a81a52e8d888d2a7391a
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-06 21:02:18 +0100

    Add unit tests for the CSS.Comments package

---
+++ regtests/css-comments-tests.ads
@@ -0,0 +1,33 @@
+-----------------------------------------------------------------------
+--  css-comments-tests -- Unit tests for CSS comments package
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+
+package CSS.Comments.Tests is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new Util.Tests.Test with null record;
+
+   --  Test comment operations on an empty comment.
+   procedure Test_Empty (T : in out Test);
+
+   --  Test inserting a comment in the comment list.
+   procedure Test_Insert (T : in out Test);
+
+end CSS.Comments.Tests;
