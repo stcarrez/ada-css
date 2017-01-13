521ae26d46527046ef5e0d74554b7c817baba4c7
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-13 20:44:44 +0100

    Implement a simple duplicate rule detection by looking at style rules and checking their properties

--- tools/csstools.adb
+++ tools/csstools.adb
@@ -28,15 +28,63 @@ with Ada.Exceptions;
 with GNAT.Traceback.Symbolic;
 with CSS.Parser.Lexer_dfa;
 with CSS.Core;
+with CSS.Core.Refs;
+with CSS.Core.Sheets;
+with CSS.Core.Vectors;
+with CSS.Core.Styles;
+with CSS.Core.Selectors;
+with CSS.Core.Properties;
 procedure CssTools is
 
    use Util.Streams.Buffered;
    use Ada.Strings.Unbounded;
 
    Count  : constant Natural := Ada.Command_Line.Argument_Count;
-   Doc    : aliased CSS.Core.Stylesheet;
+   Doc    : aliased CSS.Core.Sheets.CSSStylesheet;
+
+   procedure Report_Duplicate (Rules : CSS.Core.Vectors.Vector) is
+      use type CSS.Core.CSSRule_Type;
+      use CSS.Core.Properties;
+      use CSS.Core.Styles;
+
+      function Equal (Left, Right : in CSS.Core.Refs.Ref) return Boolean is
+         L : CSS.Core.Styles.CSSStyleRule_Access := CSS.Core.Styles.CSSStyleRule'Class (Left.Value.all)'Access;
+         R : CSS.Core.Styles.CSSStyleRule_Access := CSS.Core.Styles.CSSStyleRule'Class (Right.Value.all)'Access;
+      begin
+         return CSSProperty_List (L.Style) = CSSProperty_List (R.Style);
+      end Equal;
+
+      procedure Process (Pos : in CSS.Core.Vectors.Cursor) is
+         Rule : constant CSSStyleRule_Access := Element (Pos);
+         Iter : CSS.Core.Vectors.Cursor;
+      begin
+         if Rule = null then
+            return;
+         end if;
+         Iter := CSS.Core.Vectors.Next (Pos);
+         while CSS.Core.Vectors.Has_Element (Iter) loop
+            declare
+               Match : constant CSSStyleRule_Access := Element (Iter);
+            begin
+               if Match /= null then
+                  if CSSProperty_List (Rule.Style) = CSSProperty_List (Match.Style) then
+                     Ada.Text_IO.Put_Line ("Identical rules:");
+                     Ada.Text_IO.Put_Line ("  " & CSS.Core.To_String (Rule.Get_Location)
+                                           & ": " & CSS.Core.Selectors.To_String (Rule.Selectors));
+                     Ada.Text_IO.Put_Line ("  " & CSS.Core.To_String (Match.Get_Location)
+                                           & ": " & CSS.Core.Selectors.To_String (Match.Selectors));
+                  end if;
+               end if;
+            end;
+            CSS.Core.Vectors.Next (Iter);
+         end loop;
+      end Process;
+    begin
+       Rules.Iterate (Process'Access);
+    end Report_Duplicate;
+
 begin
-   CSS.Parser.Lexer_dfa.aflex_debug := True;
+   CSS.Parser.Lexer_dfa.aflex_debug := False;
    if Count = 0 then
       Ada.Text_IO.Put_Line ("Usage: csstools file...");
       return;
@@ -49,6 +97,7 @@ begin
       begin
          Res := CSS.Parser.Parser.Parse (S, Doc'Unchecked_Access);
          Ada.Text_IO.Put_Line ("Result: " & Integer'Image (Res));
+         Report_Duplicate (Doc.Rules);
       end;
    end loop;
    Ada.Text_IO.Put_Line ("Comments: ");
