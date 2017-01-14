916b1a4914232c14a13b3745800e08bf55948eb1
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-14 23:21:02 +0100

    - Add some documentation - Declare the Get_Value function - Declare the Append and To_String operations - Declare the Create_Xxx operation on the repository - Declare Adjust and Finalize on the Value_List

--- src/css-core-values.ads
+++ src/css-core-values.ads
@@ -15,16 +15,34 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+pragma Ada_2012;
 with Ada.Containers.Ordered_Sets;
 with Ada.Finalization;
+
+--  == CSS Values ==
+--  The <tt>CSS.Core.Values</tt> package describes types and operations to hold CSS property
+--  values.  A property value is a list of tokens some of them being strings, urls, integers,
+--  typed integers (for example dimensions) or function calls.  Each of these token is
+--  represented by the <tt>Value_Type</tt> type and the property value is represented by
+--  the <tt>Value_List</tt>.
+--
+--  The value token is created and maintained in the <tt>Repository_Type</tt> that holds all the
+--  possible values.
 package CSS.Core.Values is
 
+   --  The optional value token unit.  For example, 12px has the unit <tt>UNIT_PX</tt>.
    type Unit_Type is (UNIT_NONE, UNIT_PX, UNIT_EX, UNIT_EM,
                       UNIT_CM, UNIT_MM, UNIT_IN, UNIT_PI, UNIT_PC, UNIT_PT,
                       UNIT_DEG, UNIT_RAD, UNIT_GRAD,
                       UNIT_MS, UNIT_SEC, UNIT_HZ, UNIT_KHZ);
 
-   type Value_Kind is (VALUE_IDENT, VALUE_STRING, VALUE_URL, VALUE_COLOR);
+   --  The type of the value.
+   type Value_Kind is (VALUE_NULL,
+                       VALUE_IDENT,    --  Identifier: left, right, ...
+                       VALUE_STRING,   --  String: "..." or '...'
+                       VALUE_URL,      --  URL: url(http://...) or url("../img.png")
+                       VALUE_NUMBER,   --  Number: .2em or 10
+                       VALUE_COLOR);   --  Color: #fed or rgb(10, 20, 30)
 
    type Value_Type is private;
 
@@ -37,25 +55,53 @@ package CSS.Core.Values is
    --  Get the value unit.
    function Get_Unit (Value : in Value_Type) return Unit_Type;
 
+   --  Get the value.
+   function Get_Value (Value : in Value_Type) return String;
+
    function "<" (Left, Right : in Value_Type) return Boolean;
 
-   type Value_List is private;
+   type Value_List is tagged private;
+
+   --  Append the value to the list.
+   procedure Append (List  : in out Value_List;
+                     Value : in Value_Type);
+
+   --  Get a printable representation of the list.
+   function To_String (List : in Value_List) return String;
 
    --  Compare the two values for identity.
    function Compare (Left, Right : in Value_Type) return Boolean;
 
    --  A repository of all known values.
-   type Repository is new Ada.Finalization.Limited_Controlled with private;
+   type Repository_Type is new Ada.Finalization.Limited_Controlled with private;
 
-   --  Create a CSS selector of the given type and with the name.
-   function Create_Color (Name : in String) return Value_Type;
+   --  Create a color value.
+   function Create_Color (Repository : in out Repository_Type;
+                          Value      : in String) return Value_Type;
+
+   --  Create a URL value.
+   function Create_URL (Repository : in out Repository_Type;
+                        Value      : in String) return Value_Type;
+
+   --  Create a String value.
+   function Create_String (Repository : in out Repository_Type;
+                           Value      : in String) return Value_Type;
+
+   --  Create an identifier value.
+   function Create_Ident (Repository : in out Repository_Type;
+                          Value      : in String) return Value_Type;
+
+   --  Create a number value with an optional unit.
+   function Create_Number (Repository : in out Repository_Type;
+                           Value      : in String;
+                           Unit       : in Unit_Type := UNIT_NONE) return Value_Type;
 
 private
 
    type Value_Node;
    type Value_Type is access all Value_Node;
    type Value_Node (Len  : Natural;
-                    Kind : Value_Type) is limited record
+                    Kind : Value_Kind) is limited record
       Unit    : Unit_Type;
       Value   : String (1 .. Len);
    end record;
@@ -70,13 +116,16 @@ private
    --  Release the storage held by the selector sub-tree.
    procedure Finalize (Node : in out Value_Node);
 
-   type Repository is new Ada.Finalization.Limited_Controlled with record
+   type Repository_Type is new Ada.Finalization.Limited_Controlled with record
       Root : Value_Sets.Set;
    end record;
 
-   --  Release the selector objects that have been allocated in the tree.
+   function Create_Value (Repository : in out Repository_Type;
+                          Value      : in Value_Type) return Value_Type;
+
+   --  Release the values held by the repository.
    overriding
-   procedure Finalize (Object : in out Repository);
+   procedure Finalize (Object : in out Repository_Type);
 
    type Value_Array is array (Positive range <>) of Value_Type;
    type Value_List_Access is access all Value_List;
@@ -85,5 +134,13 @@ private
       Next   : Value_List_Access;
       Values : Value_Array (1 .. 4);
    end record;
+
+   --  Copy the value list chain if there is one.
+   overriding
+   procedure Adjust (Object : in out Value_List);
+
+   --  Release the values.
+   overriding
+   procedure Finalize (Object : in out Value_List);
 
 end CSS.Core.Values;
