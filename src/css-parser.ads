1c7a45599db3f7209a40f7dc9a4ea12bd2b53e2c
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-14 23:31:16 +0100

    Add pragma Ada_2012

--- src/css-parser.ads
+++ src/css-parser.ads
@@ -15,6 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+pragma Ada_2012;
 with Ada.Strings.Unbounded;
 with CSS.Core;
 with CSS.Core.Sheets;
