fbf48077b89f89a6b5cbcd5ab8fb996af0189f20
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-16 23:21:15 +0100

    Implement the Print and New_Line procedures to write on Text_IO file

---
+++ tools/css-printer-text_io.adb
@@ -0,0 +1,43 @@
+-----------------------------------------------------------------------
+--  css-printer -- CSS printer tools
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with CSS.Core.Properties;
+with CSS.Core.Styles;
+with Ada.Text_IO;
+package body CSS.Printer.Text_IO is
+
+   overriding
+   procedure New_Line (Stream : in out Context_Type) is
+   begin
+      Ada.Text_IO.New_Line (Stream.File);
+   end New_Line;
+
+   overriding
+   procedure Print (Stream  : in out Context_Type;
+                    Content : in String) is
+   begin
+      Ada.Text_IO.Put (Stream.File, Content);
+   end Print;
+
+   overriding
+   procedure Print (Stream  : in out Context_Type;
+                    Content : in Character) is
+   begin
+      Ada.Text_IO.Put (Stream.File, Content);
+   end Print;
+
+end CSS.Printer.Text_IO;
