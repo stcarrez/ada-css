fe476f0218b597090b8d9090885e013f1cbf1547
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-16 23:21:38 +0100

    Add some options for the tool  -d debug option  -v verbose mode  -q quiet mode  -o file to control the generation file  -p pretty print the output CSS Print a small summary or errors, warnings, duplicate rules Avoid reporting several times when a same rule is duplicated several times

--- tools/csstools.adb
+++ tools/csstools.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  json -- JSON Reader
---  Copyright (C) 2010, 2011, 2014 Stephane Carrez
+--  csstools -- CSS Tools Command
+--  Copyright (C) 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,17 +15,18 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with GNAT.Command_Line;  use GNAT.Command_Line;
+with GNAT.Traceback.Symbolic;
 with Ada.Strings.Unbounded;
 with Ada.Text_IO;
 with Ada.Command_Line;
-with Util.Serialize.IO.JSON;
 with Ada.Containers;
 with Util.Streams.Texts;
 with Util.Streams.Buffered;
 with CSS.Parser.Parser;
 with CSS.Parser.Lexer;
 with Ada.Exceptions;
-with GNAT.Traceback.Symbolic;
+with Ada.Containers.Ordered_Sets;
 with CSS.Parser.Lexer_dfa;
 with CSS.Core;
 with CSS.Core.Refs;
@@ -36,14 +37,35 @@ with CSS.Core.Selectors;
 with CSS.Core.Properties;
 with CSS.Core.Errors.Default;
 with CSS.Tools.Messages;
+with CSS.Core.Compare;
+with CSS.Printer.Text_IO;
+with System;
 procedure CssTools is
 
    use Util.Streams.Buffered;
    use Ada.Strings.Unbounded;
+   use Ada.Containers;
 
+   function "<" (Left, Right : in CSS.Core.Refs.Ref) return Boolean is
+      use System;
+   begin
+      return Left.Value.all'Address < Right.Value.all'Address;
+   end "<";
+
+   package StyleRef_Sets is
+      new Ada.Containers.Ordered_Sets (Element_Type => CSS.Core.Refs.Ref,
+                                       "=" => CSS.Core.Compare."=",
+                                       "<" => "<");
+   Debug       : Boolean := False;
+   Verbose     : Boolean := False;
+   Quiet       : Boolean := False;
    Count       : constant Natural := Ada.Command_Line.Argument_Count;
    Doc         : aliased CSS.Core.Sheets.CSSStylesheet;
    Err_Handler : aliased CSS.Tools.Messages.Message_List;
+   Output_Path : Unbounded_String;
+   Output      : CSS.Printer.Text_IO.Context_Type;
+   Dup_Count   : Natural := 0;
+   Dup_Rules   : StyleRef_Sets.Set;
 
    procedure Print (Rule : in CSS.Core.Styles.CSSStyleRule_Access) is
       procedure Print (Prop : in CSS.Core.Properties.CSSProperty) is
@@ -63,9 +85,17 @@ procedure CssTools is
    procedure Print_Message (Severity : in CSS.Tools.Messages.Severity_Type;
                             Loc      : in CSS.Core.Location;
                             Message  : in String) is
+      use CSS.Tools.Messages;
    begin
       Ada.Text_IO.Put (CSS.Core.To_String (Loc));
       Ada.Text_IO.Put (":");
+      if Severity = MSG_ERROR then
+         Ada.Text_IO.Put ("error: ");
+      elsif Severity = MSG_WARNING then
+         Ada.Text_IO.Put ("warning: ");
+      else
+         Ada.Text_IO.Put ("info: ");
+      end if;
       Ada.Text_IO.Put_Line (Message);
    end Print_Message;
 
@@ -85,22 +115,32 @@ procedure CssTools is
          Rule : constant CSSStyleRule_Access := Element (Pos);
          Iter : CSS.Core.Vectors.Cursor;
       begin
-         if Rule = null then
+         if Rule = null or else Dup_Rules.Contains (CSS.Core.Vectors.Element (Pos)) then
             return;
          end if;
          Iter := CSS.Core.Vectors.Next (Pos);
          while CSS.Core.Vectors.Has_Element (Iter) loop
             declare
+               Ref   : constant CSS.Core.Refs.Ref := CSS.Core.Vectors.Element (Iter);
                Match : constant CSSStyleRule_Access := Element (Iter);
             begin
-               if Match /= null then
+               if Match /= null and not Dup_Rules.Contains (Ref) then
                   if CSSProperty_List (Rule.Style) = CSSProperty_List (Match.Style) then
-                     Ada.Text_IO.Put_Line ("Identical rules:");
-                     Ada.Text_IO.Put_Line ("  " & CSS.Core.To_String (Rule.Get_Location)
-                                           & ": " & CSS.Core.Selectors.To_String (Rule.Selectors));
-                     Ada.Text_IO.Put_Line ("  " & CSS.Core.To_String (Match.Get_Location)
-                                           & ": " & CSS.Core.Selectors.To_String (Match.Selectors));
-		     Print (Rule);
+                     Dup_Rules.Insert (Ref);
+                     Err_Handler.Warning (Match.Get_Location, "This rule has the same properties as rule at line "
+                                          & CSS.Core.To_String (Rule.Get_Location));
+                     Err_Handler.Warning (Rule.Get_Location, "Selector '"
+                                          & CSS.Core.Selectors.To_String (Match.Selectors) & "' could be added "
+                                          & "to rule at line " & CSS.Core.To_String (Match.Get_Location));
+                     if Verbose then
+                        Ada.Text_IO.Put_Line ("Identical rules:");
+                        Ada.Text_IO.Put_Line ("  " & CSS.Core.To_String (Rule.Get_Location)
+                                             & ": " & CSS.Core.Selectors.To_String (Rule.Selectors));
+                        Ada.Text_IO.Put_Line ("  " & CSS.Core.To_String (Match.Get_Location)
+                                              & ": " & CSS.Core.Selectors.To_String (Match.Selectors));
+		        Print (Rule);
+                     end if;
+                     Dup_Count := Dup_Count + 1;
                   end if;
                end if;
             end;
@@ -112,26 +152,83 @@ procedure CssTools is
     end Report_Duplicate;
 
 begin
-   CSS.Parser.Lexer_dfa.aflex_debug := False;
-   if Count = 0 then
-      Ada.Text_IO.Put_Line ("Usage: csstools file...");
-      return;
-   end if;
+   Initialize_Option_Scan (Stop_At_First_Non_Switch => True, Section_Delimiters => "targs");
 
-   for I in 1 .. Count loop
+   Output.Compress := True;
+
+   --  Parse the command line
+   loop
+      case Getopt ("v p d q c: o: ") is
+         when ASCII.NUL => exit;
+
+         when 'c' =>
+            --  Set_Config_Directory (Parameter);
+            null;
+
+         when 'o' =>
+            Output_Path := To_Unbounded_String (Parameter);
+
+         when 'd' =>
+            Debug := True;
+
+         when 'p' =>
+            Output.Indent_Level := 4;
+            Output.Full_Semi    := True;
+            Output.Compress     := False;
+
+         when 'v' =>
+            Verbose := True;
+
+         when 'q' =>
+            Quiet := True;
+
+         when '*' =>
+            exit;
+
+         when others =>
+            null;
+      end case;
+   end loop;
+
+   CSS.Parser.Lexer_dfa.aflex_debug := Debug;
+   if Length (Output_Path) > 0 then
+      Ada.Text_IO.Create (Output.File, Ada.Text_IO.Out_File, To_String (Output_Path));
+   end if;
+   loop
       declare
-         S    : constant String := Ada.Command_Line.Argument (I);
+         Path : constant String := Get_Argument;
       begin
-         Doc.Set_Href (S);
-         CSS.Parser.Load (S, Doc'Unchecked_Access, Err_Handler'Unchecked_Access);
+         exit when Path'Length = 0;
+         Doc.Set_Href (Path);
+         CSS.Parser.Load (Path, Doc'Unchecked_Access, Err_Handler'Unchecked_Access);
          Report_Duplicate (Doc.Rules);
          Err_Handler.Iterate (Print_Message'Access);
+         if Length (Output_Path) > 0 then
+            Output.Print (Doc);
+         end if;
       end;
    end loop;
-   Ada.Text_IO.Put_Line ("Comments: ");
-   Ada.Text_IO.Put_Line (Ada.Strings.Unbounded.To_String (CSS.Parser.Lexer.Current_Comment));
+   if not Quiet then
+      Ada.Text_IO.Put_Line ("Comments: ");
+      Ada.Text_IO.Put_Line (Ada.Strings.Unbounded.To_String (CSS.Parser.Lexer.Current_Comment));
+
+      Ada.Text_IO.Put_Line ("Errors          : " & Natural'Image (Err_Handler.Get_Error_Count));
+      Ada.Text_IO.Put_Line ("Warnings        : " & Natural'Image (Err_Handler.Get_Warning_Count));
+      Ada.Text_IO.Put_Line ("CSS rules       : " & Count_Type'Image (Doc.Rules.Length));
+      Ada.Text_IO.Put_Line ("CSS values      : " & Natural'Image (Doc.Values.Length));
+      Ada.Text_IO.Put_Line ("Duplicate rules : " & Natural'Image (Dup_Count));
+   end if;
+
 exception
+   when E : Invalid_Switch =>
+      Ada.Text_IO.Put_Line ("Invalid option: " & Ada.Exceptions.Exception_Message (E));
+      --  Gen.Commands.Short_Help_Usage;
+      Ada.Command_Line.Set_Exit_Status (2);
+
    when E : others =>
+      Err_Handler.Iterate (Print_Message'Access);
+      Ada.Text_IO.Put_Line (Ada.Exceptions.Exception_Message (E));
       Ada.Text_IO.Put_Line (GNAT.Traceback.Symbolic.Symbolic_Traceback (E));
+      Ada.Command_Line.Set_Exit_Status (1);
       raise;
 end CssTools;
