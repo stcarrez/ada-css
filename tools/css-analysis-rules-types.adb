fecfb6cb68f284abbce2b3257841bbc62955ab45
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-22 11:03:19 +0100

    Fix compilation warnings

--- tools/css-analysis-rules-types.adb
+++ tools/css-analysis-rules-types.adb
@@ -20,58 +20,79 @@ package body CSS.Analysis.Rules.Types is
 
    use CSS.Core.Values;
 
+   --  ------------------------------
    --  Check if the value represents an integer without unit.
+   --  ------------------------------
    overriding
    function Match (Rule  : in Integer_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean is
+      pragma Unreferenced (Rule);
    begin
       return Get_Type (Value) = VALUE_NUMBER and Get_Unit (Value) = UNIT_NONE;
    end Match;
 
+   --  ------------------------------
    --  Check if the value represents a number or integer without unit.
+   --  ------------------------------
    overriding
    function Match (Rule  : in Number_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean is
+      pragma Unreferenced (Rule);
    begin
       return Get_Type (Value) = VALUE_NUMBER and Get_Unit (Value) = UNIT_NONE;
    end Match;
 
+   --  ------------------------------
    --  Check if the value represents a percentage.
+   --  ------------------------------
    overriding
    function Match (Rule  : in Percentage_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean is
+      pragma Unreferenced (Rule);
    begin
       return Get_Type (Value) = VALUE_NUMBER and Get_Unit (Value) = UNIT_NONE;
    end Match;
 
+   --  ------------------------------
    --  Check if the value represents a length.
+   --  ------------------------------
    overriding
    function Match (Rule  : in Length_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean is
+      pragma Unreferenced (Rule);
    begin
-      return Get_Type (Value) = VALUE_NUMBER and Get_Unit (Value) in Length_Unit_Type;      
+      return Get_Type (Value) = VALUE_NUMBER and Get_Unit (Value) in Length_Unit_Type;
    end Match;
 
+   --  ------------------------------
    --  Check if the value represents an angle.
+   --  ------------------------------
    overriding
    function Match (Rule  : in Angle_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean is
+      pragma Unreferenced (Rule);
    begin
       return Get_Type (Value) = VALUE_NUMBER and Get_Unit (Value) in Angle_Unit_Type;
    end Match;
 
+   --  ------------------------------
    --  Check if the value represents a string.
+   --  ------------------------------
    overriding
    function Match (Rule  : in String_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean is
+      pragma Unreferenced (Rule);
    begin
       return Get_Type (Value) = VALUE_STRING;
    end Match;
 
+   --  ------------------------------
    --  Check if the value represents a url.
+   --  ------------------------------
    overriding
    function Match (Rule  : in URL_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean is
+      pragma Unreferenced (Rule);
    begin
       return Get_Type (Value) = VALUE_URL;
    end Match;
