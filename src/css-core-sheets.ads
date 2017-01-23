0ad334ba4deb5c324ec93148cd054bf83a0e594d
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-23 21:35:48 +0100

    Declare the Create_Rule and Append operation to create and populate a media rule

--- src/css-core-sheets.ads
+++ src/css-core-sheets.ads
@@ -20,6 +20,7 @@ with CSS.Core.Styles;
 with CSS.Core.Vectors;
 with CSS.Core.Values;
 with CSS.Core.Properties;
+with CSS.Core.Medias;
 package CSS.Core.Sheets is
 
    type CSSStylesheet is new CSS.Core.StyleSheet with record
@@ -31,7 +32,24 @@ package CSS.Core.Sheets is
    --  Create a CSS rule.
    function Create_Rule (Document : in CSSStyleSheet) return Styles.CSSStyleRule_Access;
 
+   --  Create a CSS media rule.
+   function Create_Rule (Document : in CSSStyleSheet) return Medias.CSSMediaRule_Access;
+
+   --  Append the CSS rule to the document.
    procedure Append (Document : in out CSSStylesheet;
+                     Rule     : in Styles.CSSStyleRule_Access;
+                     Line     : in Natural;
+                     Column   : in Natural);
+
+   --  Append the media rule to the document.
+   procedure Append (Document : in out CSSStylesheet;
+                     Rule     : in Medias.CSSMediaRule_Access;
+                     Line     : in Natural;
+                     Column   : in Natural);
+
+   --  Append the CSS rule to the media.
+   procedure Append (Document : in out CSSStylesheet;
+                     Media    : in Medias.CSSMediaRule_Access;
                      Rule     : in Styles.CSSStyleRule_Access;
                      Line     : in Natural;
                      Column   : in Natural);
