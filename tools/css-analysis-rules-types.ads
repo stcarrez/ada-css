4753945ec6f61f32a164f6787891e86e2722a49a
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-26 21:25:22 +0100

    Declare the Register procedure to register the builtin types in the repository Change the pre-defined types to private types Declare Builtin_Rule_Type as root for builtin types Override the Print procedure to report the builtin type name New Time_Rule_Type type

--- tools/css-analysis-rules-types.ads
+++ tools/css-analysis-rules-types.ads
@@ -15,70 +15,104 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+private with Ada.Strings.Unbounded;
 package CSS.Analysis.Rules.Types is
 
-   type Integer_Rule_Type is new Rule_Type with null record;
+   --  Register all the builtin types.
+   procedure Register (Repository : in out Repository_Type);
+
+private
+
+   type Builtin_Rule_Type is new Rule_Type with record
+      Name : Ada.Strings.Unbounded.Unbounded_String;
+   end record;
+   type Builtin_Rule_Type_Access is access all Builtin_Rule_Type'Class;
+
+   --  Print the rule definition to the print stream.
+   procedure Print (Rule   : in Builtin_Rule_Type;
+                    Stream : in out CSS.Printer.File_Type'Class);
+
+   type Integer_Rule_Type is new Builtin_Rule_Type with null record;
 
    --  Check if the value represents an integer without unit.
    overriding
    function Match (Rule  : in Integer_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean;
 
-   type Number_Rule_Type is new Rule_Type with null record;
+   type Number_Rule_Type is new Builtin_Rule_Type with null record;
 
    --  Check if the value represents a number or integer without unit.
    overriding
    function Match (Rule  : in Number_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean;
 
-   type Percentage_Rule_Type is new Rule_Type with null record;
+   type Percentage_Rule_Type is new Builtin_Rule_Type with null record;
 
    --  Check if the value represents a percentage.
    overriding
    function Match (Rule  : in Percentage_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean;
 
-   type Length_Rule_Type is new Rule_Type with null record;
+   type Length_Rule_Type is new Builtin_Rule_Type with null record;
 
    --  Check if the value represents a length.
    overriding
    function Match (Rule  : in Length_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean;
 
-   type Angle_Rule_Type is new Rule_Type with null record;
+   type Time_Rule_Type is new Builtin_Rule_Type with null record;
+
+   --  Check if the value represents a time.
+   overriding
+   function Match (Rule  : in Time_Rule_Type;
+                   Value : in CSS.Core.Values.Value_Type) return Boolean;
+
+   type Resolution_Rule_Type is new Builtin_Rule_Type with null record;
+
+   --  Check if the value represents a resolution.
+   overriding
+   function Match (Rule  : in Resolution_Rule_Type;
+                   Value : in CSS.Core.Values.Value_Type) return Boolean;
+
+   type Angle_Rule_Type is new Builtin_Rule_Type with null record;
 
    --  Check if the value represents an angle.
    overriding
    function Match (Rule  : in Angle_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean;
 
-   type String_Rule_Type is new Rule_Type with null record;
+   type String_Rule_Type is new Builtin_Rule_Type with null record;
 
    --  Check if the value represents a string.
    overriding
    function Match (Rule  : in String_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean;
 
-   type Identifier_Rule_Type is new Rule_Type with null record;
+   type Identifier_Rule_Type is new Builtin_Rule_Type with null record;
 
    --  Check if the value represents a custom identifier.
    overriding
    function Match (Rule  : in Identifier_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean;
 
-   type URL_Rule_Type is new Rule_Type with null record;
+   type URL_Rule_Type is new Builtin_Rule_Type with null record;
 
    --  Check if the value represents a string.
    overriding
    function Match (Rule  : in URL_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean;
 
-   type Color_Rule_Type is new Rule_Type with null record;
+   type Color_Rule_Type is new Builtin_Rule_Type with null record;
 
    --  Check if the value represents an hexadecimal color.
    overriding
    function Match (Rule  : in Color_Rule_Type;
                    Value : in CSS.Core.Values.Value_Type) return Boolean;
+
+   --  Register the builtin type in the repository.
+   procedure Register_Builtin (Repository : in out Repository_Type;
+                               Name       : in String;
+                               Rule       : in Builtin_Rule_Type_Access;
+                               Kind       : in CSS.Core.Values.Value_Kind);
 
 end CSS.Analysis.Rules.Types;
