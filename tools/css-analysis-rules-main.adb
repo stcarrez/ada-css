55efb743d63573f1b8f3ee5d2c25e549a15c86bc
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-26 21:38:51 +0100

    Implement the operations for the main repository instance

---
+++ tools/css-analysis-rules-main.adb
@@ -0,0 +1,40 @@
+-----------------------------------------------------------------------
+--  css-analysis-rules-main -- Rule repository
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+package body CSS.Analysis.Rules.Main is
+
+   Repo : aliased Repository_Type;
+
+   --  ------------------------------
+   --  Get the rule repository instance.
+   --  ------------------------------
+   function Rule_Repository return access Repository_Type is
+   begin
+      return Repo'Access;
+   end Rule_Repository;
+
+   --  ------------------------------
+   --  Analyze the CSS rules with their properties against the repository
+   --  definition and report any warning or error.
+   --  ------------------------------
+   procedure Analyze (Sheet      : in CSS.Core.Sheets.CSSStylesheet;
+                      Report     : in out CSS.Core.Errors.Error_Handler'Class) is
+   begin
+      Analyze (Repo, Sheet, Report);
+   end Analyze;
+
+end CSS.Analysis.Rules.Main;
