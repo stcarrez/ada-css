a170da40d275cc78516cc74b772fab28f386f8da
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-01-27 21:37:59 +0100

    Declare the CSS.Reports.Docs package with the Print procedure to report the class analysis information

---
+++ tools/css-reports-docs.ads
@@ -0,0 +1,25 @@
+-----------------------------------------------------------------------
+--  css-reports -- CSS Reports
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with CSS.Printer;
+with CSS.Analysis.Classes;
+package CSS.Reports.Docs is
+
+   procedure Print (Stream : in out CSS.Printer.File_Type'Class;
+                    Data   : in CSS.Analysis.Classes.Map);
+
+end CSS.Reports.Docs;
