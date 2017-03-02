371bfe547ab1d7837a85609069b0bb6c114e70b2
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-03-02 21:42:57 +0100

    New tests for the CSS printer

---
+++ regtests/css-printer-tests.ads
@@ -0,0 +1,42 @@
+-----------------------------------------------------------------------
+--  css-print-tests -- Unit tests for CSS printer
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Strings.Unbounded;
+with Util.Tests;
+
+package CSS.Printer.Tests is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new Util.Tests.Test_Case with record
+      Name      : Ada.Strings.Unbounded.Unbounded_String;
+      File      : Ada.Strings.Unbounded.Unbounded_String;
+      Expect    : Ada.Strings.Unbounded.Unbounded_String;
+      Result    : Ada.Strings.Unbounded.Unbounded_String;
+      Compress  : Boolean := False;
+   end record;
+   type Test_Case_Access is access all Test;
+
+   --  Test case name
+   overriding
+   function Name (T : Test) return Util.Tests.Message_String;
+
+   --  Perform the test.
+   overriding
+   procedure Run_Test (T : in out Test);
+
+end CSS.Printer.Tests;
