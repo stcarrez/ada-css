1647dd3f3c7e53f6a1d78773a086fb162c8bb7f1
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-03-02 21:43:25 +0100

    Implement the CSS printer tests

---
+++ regtests/css-printer-tests.adb
@@ -0,0 +1,150 @@
+-----------------------------------------------------------------------
+--  css-printer-tests -- Unit tests for CSS printer
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Text_IO;
+with Ada.Directories;
+
+with Util.Measures;
+
+with CSS.Core.Sheets;
+with CSS.Core.Errors.Default;
+with CSS.Parser;
+with CSS.Printer.Text_IO;
+package body CSS.Printer.Tests is
+
+   use Ada.Strings.Unbounded;
+
+   --  ------------------------------
+   --  Test case name
+   --  ------------------------------
+   overriding
+   function Name (T : in Test) return Util.Tests.Message_String is
+   begin
+      return Util.Tests.Format ("Test CSS printer " & To_String (T.Name));
+   end Name;
+
+   --  ------------------------------
+   --  Perform the test.
+   --  ------------------------------
+   overriding
+   procedure Run_Test (T : in out Test) is
+      Name   : constant String := Ada.Strings.Unbounded.To_String (T.Name);
+      Path   : constant String := Ada.Strings.Unbounded.To_String (T.File);
+      Doc    : aliased CSS.Core.Sheets.CSSStylesheet;
+      Errors : aliased CSS.Core.Errors.Default.Error_Handler;
+      Output : CSS.Printer.Text_IO.File_Type;
+   begin
+      Doc.Set_Href (Name);
+      Output.Compress := T.Compress;
+      if not T.Compress then
+         Output.Indent_Level := 4;
+         Output.Full_Semi := True;
+      end if;
+      Ada.Text_IO.Create (Output.File, Ada.Text_IO.Out_File, To_String (T.Result));
+      declare
+         Time : Util.Measures.Stamp;
+      begin
+         CSS.Parser.Load (Path, Doc'Unchecked_Access, Errors'Unchecked_Access);
+         Util.Measures.Report (Time, "CSS parse " & Name);
+      end;
+      Output.Print (Doc);
+      Ada.Text_IO.Close (Output.File);
+      Util.Tests.Assert_Equals (T, 0, Errors.Error_Count,
+                                "Errors reported for '" & Name & "'");
+      Util.Tests.Assert_Equal_Files (T, Ada.Strings.Unbounded.To_String (T.Expect),
+                                     To_String (T.Result), "Print of " & Name & " failed");
+
+   exception
+      when others =>
+         if Ada.Text_IO.Is_Open (Output.File) then
+            Ada.Text_IO.Close (Output.File);
+         end if;
+         raise;
+
+   end Run_Test;
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite) is
+      use Ada.Directories;
+
+      procedure Add_Parser_Tests (Dir       : in String;
+                                  Compress  : in Boolean);
+
+      function Create_Test (Name    : in String;
+                            Path    : in String;
+                            Prefix  : in String) return Test_Case_Access;
+
+      Result_Dir  : constant String := "regtests/result/";
+      Expect_Dir  : constant String := "regtests/expect/";
+      Expect_Path : constant String := Util.Tests.Get_Path (Expect_Dir);
+      Result_Path : constant String := Util.Tests.Get_Test_Path (Result_Dir);
+      Search      : Search_Type;
+      Filter      : constant Filter_Type := (others => True);
+      Ent         : Directory_Entry_Type;
+
+      function Create_Test (Name    : in String;
+                            Path    : in String;
+                            Prefix  : in String) return Test_Case_Access is
+         Tst    : Test_Case_Access;
+      begin
+         Tst := new Test;
+         Tst.Name    := To_Unbounded_String (Name);
+         Tst.File    := To_Unbounded_String (Path);
+         Tst.Expect  := To_Unbounded_String (Expect_Path & Prefix & Name);
+         Tst.Result  := To_Unbounded_String (Result_Path & Name);
+         return Tst;
+      end Create_Test;
+
+      procedure Add_Parser_Tests (Dir       : in String;
+                                  Compress  : in Boolean) is
+         Path        : constant String := Util.Tests.Get_Path (Dir);
+      begin
+         if Kind (Path) /= Directory then
+            Ada.Text_IO.Put_Line ("Cannot read test directory: " & Path);
+         end if;
+
+         Start_Search (Search, Directory => Path, Pattern => "*.css", Filter => Filter);
+         while More_Entries (Search) loop
+            Get_Next_Entry (Search, Ent);
+            declare
+               Simple : constant String := Simple_Name (Ent);
+               Name   : constant String := Base_Name (Simple);
+               Tst    : Test_Case_Access;
+            begin
+               if Simple /= "." and then Simple /= ".."
+                 and then Simple /= ".svn" and then Simple (Simple'Last) /= '~'
+               then
+                  if Compress then
+                     Tst := Create_Test (Name, Path & "/" & Simple, "compress/");
+                  else
+                     Tst := Create_Test (Name, Path & "/" & Simple, "printer/");
+                  end if;
+                  if Tst /= null then
+                     Tst.Compress := Compress;
+                     Suite.Add_Test (Tst.all'Access);
+                  end if;
+               end if;
+            end;
+         end loop;
+      end Add_Parser_Tests;
+
+   begin
+      Add_Parser_Tests ("regtests/files/printer", False);
+      Add_Parser_Tests ("regtests/files/compress", True);
+   end Add_Tests;
+
+end CSS.Printer.Tests;
