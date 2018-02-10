739503d64e669c10e00b7dc2e41c07c3e0af4236
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-02-10 18:07:08 +0100

    New package CSS.Commands for the definition of the csstools commands

---
+++ tools/css-commands.ads
@@ -0,0 +1,51 @@
+-----------------------------------------------------------------------
+--  css-commands -- Commands for CSS tools
+--  Copyright (C) 2018 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Util.Commands.Drivers;
+with CSS.Printer.Text_IO;
+with CSS.Core.Sets;
+with CSS.Core.Sheets;
+with CSS.Tools;
+with CSS.Analysis.Classes;
+package CSS.Commands is
+
+   type Context_Type is limited record
+      Doc         : aliased CSS.Core.Sheets.CSSStylesheet;
+      Err_Handler : aliased CSS.Tools.Messages.Message_List;
+      Output_Path : Unbounded_String;
+      Report_Path : Unbounded_String;
+      Config_Dir  : Unbounded_String;
+      Output      : CSS.Printer.Text_IO.File_Type;
+      Report      : CSS.Printer.Text_IO.File_Type;
+      Dup_Rules   : CSS.Core.Sets.Set;
+      Class_Map   : CSS.Analysis.Classes.Class_Maps.Map;
+   end record;
+
+   package Drivers is
+     new Util.Commands.Drivers (Context_Type => Context_Type,
+                                Driver_Name  => "gen-commands");
+
+   subtype Command is Drivers.Command_Type;
+   subtype Command_Access is Drivers.Command_Access;
+   subtype Argument_List is Util.Commands.Argument_List;
+
+   Driver : Drivers.Driver_Type;
+
+   --  Print csstools short usage.
+   procedure Short_Help_Usage;
+
+end CSS.Commands;
