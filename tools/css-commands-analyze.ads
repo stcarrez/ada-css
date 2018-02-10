1e2db9d8948dab212bbd621e56db5798c64af5a9
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-02-10 20:51:51 +0100

    New command to analyze the CSS

---
+++ tools/css-commands-analyze.ads
@@ -0,0 +1,38 @@
+-----------------------------------------------------------------------
+--  css-commands-analyze -- List command for CSS tools
+--  Copyright (C) 2018 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+package CSS.Commands.Analyze is
+
+   --  ------------------------------
+   --  Analyze Command
+   --  ------------------------------
+   --  This command analyzes the CSS files and provides reports.
+   type Command is new CSS.Commands.Command with null record;
+
+   --  Execute the command with the arguments.
+   overriding
+   procedure Execute (Cmd       : in Command;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out CSS.Commands.Context_Type);
+
+   --  Write the help associated with the command.
+   overriding
+   procedure Help (Cmd       : in Command;
+                   Context : in out CSS.Commands.Context_Type);
+
+end CSS.Commands.Analyze;
