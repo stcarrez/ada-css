1c14c4679ffb7413183cc840f862b01acce2c1e1
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-02-10 23:17:41 +0100

    List the CSS class names for the 'list' command List the CSS colors for the 'list-colors' command

--- tools/css-commands-list.adb
+++ tools/css-commands-list.adb
@@ -16,6 +16,7 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 with Ada.Text_IO;
+with CSS.Core.Values;
 package body CSS.Commands.List is
 
    --  ------------------------------
@@ -26,20 +27,42 @@ package body CSS.Commands.List is
                       Name    : in String;
                       Args    : in Argument_List'Class;
                       Context : in out CSS.Commands.Context_Type) is
+      pragma Unreferenced (Cmd);
+      use type CSS.Core.Values.Value_Kind;
+      procedure Process (Pos : in CSS.Analysis.Classes.Cursor);
+      procedure Process_Value (Value : in CSS.Core.Values.Value_Type);
+
+      Console : constant CSS.Commands.Console_Access := Context.Console;
+
       procedure Process (Pos : in CSS.Analysis.Classes.Cursor) is
       begin
          Ada.Text_IO.Put_Line (CSS.Analysis.Classes.Class_Maps.Key (Pos));
       end Process;
+
+      procedure Process_Value (Value : in CSS.Core.Values.Value_Type) is
+      begin
+         if CSS.Core.Values.Get_Type (Value) = CSS.Core.Values.VALUE_COLOR then
+            Ada.Text_IO.Put_Line (CSS.Core.Values.To_String (Value));
+         end if;
+      end Process_Value;
    begin
       CSS.Commands.Load (Args, Context);
-      CSS.Analysis.Classes.Analyze (Context.Doc, Context.Class_Map, Context.Err_Handler);
-      Context.Class_Map.Iterate (Process'Access);
+      if Name = "list" then
+         CSS.Analysis.Classes.Analyze (Context.Doc, Context.Class_Map, Context.Err_Handler);
+         Context.Class_Map.Iterate (Process'Access);
+      end if;
+      if Name = "list-colors" then
+         Context.Doc.Values.Iterate (Process_Value'Access);
+      end if;
    end Execute;
 
+   --  ------------------------------
    --  Write the help associated with the command.
+   --  ------------------------------
    overriding
    procedure Help (Cmd     : in Command;
                    Context : in out CSS.Commands.Context_Type) is
+      pragma Unreferenced (Cmd);
       Console : constant CSS.Commands.Console_Access := Context.Console;
    begin
       Console.Notice (N_HELP, "list: list information about the CSS files");
